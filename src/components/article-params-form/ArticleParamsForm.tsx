b8f1cf4825271d6aeb5a5c7970c88b6707d52f1a
Ilshat Mustafin <iltafi@yandex.ru>
2026-06-08 21:44:51 +0500

    feat: реализовано открытие сайдбара и закрытие по клику извне

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,49 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState, useRef, useEffect } from 'react';
+import { clsx } from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	// Привяжем реф непосредственно к выезжающей панели aside
+	const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+	// Иверсия состояния меню по стрелке
+	const handleToggle = () => {
+		setIsOpen((prev) => !prev);
+	};
+	// Закрытие меню по нажатию вне области
+	useEffect(() => {
+		if (!isOpen) return;
+
+		const handleOutsideClick = (event: MouseEvent) => {
+			const target = event.target as Node;
+			// Если нажали внутри сайдбара
+			if (sidebarRef.current && sidebarRef.current.contains(target)) {
+				return;
+			}
+			// Проверяем клика по самой кнопке-стрелке
+			if ((target as HTMLElement).closest('[role="button"]')) {
+				return;
+			}
+			// Иначе закрываем форму
+			setIsOpen(false);
+		};
+		window.addEventListener('mousedown', handleOutsideClick);
+		// Удаляем слушатель
+		return () => {
+			window.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handleToggle} />
+			<aside ref={sidebarRef} className={clsx(styles.container, {
+				[styles.container_open]: isOpen,
+			})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
